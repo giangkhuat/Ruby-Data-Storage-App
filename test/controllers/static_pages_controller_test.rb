5e53d71c5a2a15125b64e38164d87c9aa40e7dc7
giang khuat <khuatgia@grinnell.edu>
2020-09-15 01:57:28 +0000

    Set up signup route

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,19 +14,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   
   test "should get home" do
-    get static_pages_home_url
+    #get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
   
   test "Should get about" do
-    get static_pages_about_url
+    #get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
   
    test "Should get contact" do
-    get static_pages_contact_url
+    #get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
