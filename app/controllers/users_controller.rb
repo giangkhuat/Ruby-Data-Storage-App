3bec9f8fd4b0a809348a4b3f4f8e66d8a2cb09f8
giang khuat <khuatgia@grinnell.edu>
2020-09-20 16:12:48 +0000

    Finishs signup forms and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
       render 'new'
